deeb6a1b2aaaa6ae727f806f95335a2f445752f8
babi-s4ntos <Nadiaferreirasantos78@gmail.com>
2025-11-02 16:31:12 -0300

    atv1

---
+++ src/main/java/br/com/fatec/atv_1/controller/NomeController.java
@@ -0,0 +1,17 @@
+package br.com.fatec.atv_1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+public class NomeController {
+
+    @GetMapping("/nome")
+    public String retornaNome(){
+        return "Barbara Santos";
+    }
+
+}
